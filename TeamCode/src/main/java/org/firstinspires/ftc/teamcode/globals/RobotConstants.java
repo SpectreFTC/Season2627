a88abb5add3a73c130fcabd38f827fa7e3088eba
Minessh Uchat <minessh.uchat@gmail.com>
2026-09-20 10:42:54 -0500

    Robot Constants file

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/globals/RobotConstants.java
@@ -0,0 +1,9 @@
+package org.firstinspires.ftc.teamcode.globals;
+
+public interface RobotConstants {
+
+    //TODO: Tune these vals, add more servo positions if needed
+    double intakePower = 0.55;
+    double servoUpPos = 0.8;
+    double servoDownPos = 0.2;
+}
